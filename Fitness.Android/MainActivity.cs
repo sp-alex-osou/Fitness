a3d9cdcc7587451e09e97341a5b665f844a4d171
Alexander Osou <alex.osou@gmx.at>
2015-06-27 01:16:12 +0200

    first commit

---
+++ Fitness.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Fitness.Shared;
+
+namespace Fitness.Android
+{
+	[Activity (Label = "Fitness.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		//int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			AuthenticationManager.Init();
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+			button.Click += async delegate {
+				button.Text = "sending request...";
+				var success = await AuthenticationManager.Register("alex@osou.at", "yeah");
+
+				button.Text = success ? "yeah!" : "meh :(";
+				//button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
---
+++ Fitness.Shared/AuthenticationManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Fitness.Shared
+{
+	public static class AuthenticationManager
+	{
+		public static void Init()
+		{
+			ServicePointManager.ServerCertificateValidationCallback += delegate
+			{
+				return true;
+			};
+		}
+
+		public static async Task<bool> Register(string email, string password)
+		{
+			var parameters = new Dictionary<string, string>
+			{
+				{ "username", "alex" },
+				{ "password", "yeah" }
+			};
+
+			var content = new FormUrlEncodedContent(parameters);
+
+			HttpResponseMessage result;
+
+			using (var client = new HttpClient())
+			{
+				try
+				{
+					result = await client.PostAsync("https://192.168.1.104:44302/Authentication/Register", content);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("ERROR: " + e.Message);
+					return false;
+				}
+			}
+
+			if (result.StatusCode != HttpStatusCode.OK)
+			{
+				Console.WriteLine("ERROR: " + result.StatusCode);
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
